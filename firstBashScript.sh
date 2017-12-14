34f11f19ce87cb3d24a9c1c0201cdfee18588142
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2017-12-14 12:19:24 +0000

    2017-12-14 1219: Added some basic log handling

--- firstBashScript.sh
+++ firstBashScript.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+dateTime="$(date +%Y-%m-%d_%H%M%S)"
+logFile="$HOME/logs/firstBashScript_log_$dateTime.txt"
 echo "Updating your machine now..."
-apt-get update && apt-get upgrade -y
+(apt-get update && apt-get upgrade -y) >> $logFile
 echo "Done updating"
+
