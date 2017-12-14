a9cf166279f9d5c30b58e2a8bb2046146cd0f3df
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2017-12-14 13:02:19 +0000

    2017-12-14 1301: Initial working update script including basic log file and directory handling

--- firstBashScript.sh
+++ firstBashScript.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 dateTime="$(date +%Y-%m-%d_%H%M%S)"
-logFile="$HOME/logs/firstBashScript_log_$dateTime.txt"
+logDirectory="$HOME/logs"
+logFile="firstBashScript_log_$dateTime.txt"
+if test ! -d $logDirectory
+	then
+		mkdir $logDirectory
+fi
 echo "Updating your machine now..."
-(apt-get update && apt-get upgrade -y) >> $logFile
+(apt-get update && apt-get upgrade -y) >> $logDirectory/$logFile
 echo "Done updating"
 
