1debd23f38a01d548ea1b01de92149a9becc62ec
Graham Watts <34165628+GingerGraham@users.noreply.github.com>
2018-02-07 22:30:15 +0000

    01. Changed log file to a .log extension 02. Added echoing to the log for start and end date and time

--- systemUpdate.sh
+++ systemUpdate.sh
@@ -2,7 +2,7 @@
 # Define variables to be used
 dateTime="$(date +%Y-%m-%d_%H%M%S)"
 logDirectory="/var/log/systemUpdate/"
-logFile="systemUpdate_log_$dateTime.txt"
+logFile="systemUpdate_log_$dateTime.log"
 
 # Test if the log directory exists
 if [ ! -d $logDirectory ];
@@ -18,6 +18,17 @@ if [ ! -f $logDirectory$logFile ];
 		touch $logFile
 fi
 
+# Confirm we're starting to update the machine now
 echo "Updating your machine now..."
+
+# Echo script start time to log
+echo "Update started at $dateTime" >> $logDirectory$logFile
+
+# Run apt-get commands to update repos and apply any relevant updates
 (apt-get update && apt-get upgrade -y) >> $logDirectory$logFile
+
+# Echo completetion time to log
+echo "Update completed at $(date +%Y-%m-%d_%H%M%S)" >> $logDirectory$logFile
+
+# Confirm update has finished
 echo "Done updating"
